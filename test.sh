7bf29bd9c21b9724e1103f35ad1e8b4b93b5d0ce
Sean <sean_carrico@yahoo.com>
2016-08-25 15:26:24 -0600

    add env echo stuff

---
+++ test.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+TRAVIS_BRANCH=dev
+function getJsonVal () {
+    python -c "import json,sys;sys.stdout.write(json.dumps(json.load(sys.stdin)$1, sort_keys=True, indent=4))";
+}
+
+prodRE='master'
+
+if [[ ${TRAVIS_BRANCH} =~ $prodRE ]]; then
+    BUILD_ENV="prod"
+else
+    BUILD_ENV="test"
+fi
+
+echo ${BUILD_ENV}
+echo ${TRAVIS_BRANCH}
